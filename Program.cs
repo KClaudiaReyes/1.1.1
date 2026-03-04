b8e036ba70ca735bbf552b97763fbb51434bdd18
clauR <creyesccf@gmail.com>
2026-03-04 16:25:09 +0100

    estudiantinicio

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace _1._1
     {
         static void Main(string[] args)
         {
+            Dictionary <int , string > estudiantes = new Dictionary<int, string> ();
+
+<<<<<<< HEAD
+            estudiantes.Add(5001, "James Carter");
+            estudiantes.Add(1995, " Bruno Mendez");
+            estudiantes.Add(2026, "Carla Hernandez");
+
+
+=======
+>>>>>>> 46cbec97ac93db69bc641206602a6018a0e067fa
         }
     }
 }
